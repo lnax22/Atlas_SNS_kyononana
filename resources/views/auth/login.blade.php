16e5c22e62d3acc5795ea767fe83282b2bbe54b5
なな <nk.duffy00kara7@gmail.com>
2023-06-06 16:55:52 +0900

    レイアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,14 @@
 
 <p>AtlasSNSへようこそ</p>
 
+<div class = loginPage>
 {{ Form::label('mail address') }}
 {{ Form::text('mail',null,['class' => 'input']) }}
 {{ Form::label('password') }}
 {{ Form::password('password',['class' => 'input']) }}
 
 {{ Form::submit('Login') }}
+</div>
 
 <p><a href="/register">新規ユーザーの方はこちら</a></p>
 
